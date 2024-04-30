3131f60b926a08b6358c7d7100c5c3dec696f794
Thierno <77080504+thierno953@users.noreply.github.com>
2024-04-30 11:26:31 +0200

    Kubeadm Cluster

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kubeadm Cluster Setup Scripts
---
+++ configs/join.sh
@@ -0,0 +1,1 @@
+kubeadm join 192.168.56.100:6443 --token 47r8f9.c2dsy07f987i8yes --discovery-token-ca-cert-hash sha256:b3c4fe8c9613e1673088442ac706ae2288a3772c2164d251eafb1fbbbd8c1c6c 
